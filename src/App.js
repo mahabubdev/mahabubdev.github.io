7f0c3b8173231f27f25383d66a666f1aec12c92c
Md Mahabub Alam <mahabub2000bd@gmail.com>
2021-05-04 16:34:31 +0600

    gh-page-redirect-update

--- src/App.js
+++ src/App.js
@@ -7,20 +7,4 @@ function App() {
 }
 
 
-// gh-pages redirect handle
-(function(){
-  var redirect = window.sessionStorage.redirect;
-  delete window.sessionStorage.redirect;
-  if (redirect && redirect !== window.location.href) {
-    // history.replaceState(null, null, redirect);
-    window.history.replaceState(null, null, redirect);
-    // REMOVE THIS - just showing the redirect route in the UI
-    // document.body.setAttribute('message', 'This page was redirected by 404.html, from the route: ' + redirect);
-  }
-  else {
-    // REMOVE THIS - just showing the redirect route in the UI
-    // document.body.setAttribute('message', 'This page was loaded directly from the index.html file');
-  }
-})();
-
 export default App;
